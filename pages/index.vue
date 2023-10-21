768a35263839871eeb40c009aa823e78ce4a591d
Noppadon Tongtrakoon <notenoppadon5019@gmail.com>
2023-10-22 00:02:38 +0700

    test fetch

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello
+    {{  appConfig }}
+    <div v-for="todo in todos">
+<span>{{ todo.task }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const appConfig = useAppConfig()
+const { data: todos } = await useFetch('https://6533ed4fe1b6f4c59046646a.mockapi.io/api/v1/todos')
+console.log(todos.value);
+</script>
